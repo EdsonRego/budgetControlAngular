3591027d837289b41a96fcaaa68972fa2f0b7470
EdsonRego <edsonxrego@gmail.com>
2024-03-05 19:42:23 -0300

    Cretead the components: Ingreso, Egreso, Cabecera y formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './cabecero/cabecero.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { EgresoComponent } from './egreso/egreso.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    IngresoComponent,
+    EgresoComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
